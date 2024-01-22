2d07f65489e445db3f17003e3eb1c32bb93fc454
Melissa Baker <mtfourcandles@gmail.com>
2024-01-22 13:00:52 +0000

    remove framer

--- my_portfolio/app/layout.tsx
+++ my_portfolio/app/layout.tsx
@@ -2,9 +2,9 @@ import Header from '../components/header';
 // import Footer from '../compontents/footer';
 import ThemeSwitch from '../components/theme-switch';
 import ThemeContextProvider from '../context/theme-context';
-// import ActiveSectionContextProvider from '../context/active-section-context';
+import ActiveSectionContextProvider from '../context/active-section-context';
 import './global.css';
-// import createContext from '../context/active-section-context';
+import createContext from '../context/active-section-context';
 
 
 const font = Playfair({ subsets: ['latin'] });
@@ -23,7 +23,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       {/* <ActiveSectionContextProvider>   */}
       <ThemeContextProvider>
       <ThemeSwitch/>
-      <Banner  />
           <Header/>
             {children}
           </ThemeContextProvider>
