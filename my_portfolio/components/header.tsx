739265c79829cae14574f400a37b3d62f705ace3
Melissa Baker <mtfourcandles@gmail.com>
2024-01-22 13:13:56 +0000

    tranistion alteration

--- my_portfolio/components/header.tsx
+++ my_portfolio/components/header.tsx
@@ -1,10 +1,11 @@
+'use client';
+
 import React from 'react';
 import { links } from '../lib/data';
-import { motion } from 'framer-motion';
 import Link from 'next/link';
 import clsx from 'clsx';
 import { useActiveSectionContext } from '../context/active-section-context';
-
+import { CSSTransition } from 'react-transition-group';
 
 
 
@@ -14,15 +15,15 @@ export default function Header() {
 
   return (
     <header >
-      <motion.div
+      <CSSTransition
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
-      ></motion.div>
+      ></CSSTransition>
 
       <nav className="flex gap-4 fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
-            <motion.li
+            <CSSTransition
               className="h-3/4 flex items-center justify-center relative"
               key={link.hash}
               initial={{ y: -100, opacity: 0 }}
@@ -45,7 +46,7 @@ export default function Header() {
                 {link.name}
 
                 {link.name === activeSection && (
-                  <motion.span
+                  <CSSTransition
                     className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
                     layoutId="activeSection"
                     transition={{
@@ -53,10 +54,10 @@ export default function Header() {
                       stiffness: 380,
                       damping: 30,
                     }}
-                  ></motion.span>
+                  ></CSSTransition>
                 )}
               </Link>
-            </motion.li>
+            </CSSTransition>
           ))}
         </ul>
       </nav>
